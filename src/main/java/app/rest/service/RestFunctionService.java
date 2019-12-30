f80eb22db46afcb82a5fc1e1b73e52bbc78ec6f1
skuarch <skuarch@yahoo.com.mx>
2019-12-30 11:07:42 -0600

    Update RestFunctionService

--- src/main/java/app/rest/service/RestFunctionService.java
+++ src/main/java/app/rest/service/RestFunctionService.java
@@ -1,10 +1,7 @@
 package app.rest.service;
 
-import app.rest.domain.entity.GitHubUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 import java.util.function.Function;
 
 @Service
